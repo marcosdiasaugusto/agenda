633ae1725a5fff913769dd1a187a40a4b4d74102
Marcos Augusto Dias <maugustod@gmail.com>
2021-11-28 05:55:59 -0300

    Criada tela de listagem de eventos.

--- agenda/urls.py
+++ agenda/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core import views
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('agenda/',views.lista_eventos),
+    path('', RedirectView.as_view(url='/agenda/')) # http://127.0.0.1:8000/agenda/
+    # path('', views.index)
 ]
